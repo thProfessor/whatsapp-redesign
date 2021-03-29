eb8e18ed3a915a8520f4a0d8e4807288a8ffea03
thProfessor <utkrs.sukla99@gmail.com>
2021-03-29 14:13:25 +0530

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
-import Login from "./Login";
+import Login from "./login";
 import Loading from "../components/Loading";
 import { useEffect } from "react";
 import firebase from "firebase";
